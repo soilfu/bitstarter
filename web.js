a7d17fbf2f80a99a818f3cd9838e5efa843c898f
Anna.Rozanova <soilfu@gmail.com>
2013-07-11 15:48:06 +0000

    Updated 4 web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
-  response.send(buffer.toString()));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
